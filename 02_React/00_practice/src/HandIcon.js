90e2addefea0ee1e923582fe511459d0f9ddcfd2
MunYong <gjansdyd@gmail.com>
2022-12-26 14:03:50 +0900

    연습문제 풀이

---
+++ 02_React/00_practice/src/HandIcon.js
@@ -0,0 +1,14 @@
+import rockImg from './assets/rock.svg';
+import scissorImg from './assets/scissor.svg';
+import paperImg from './assets/paper.svg';
+
+const IMAGES = {
+  rock: rockImg,
+  scissor: scissorImg,
+  paper: paperImg
+}
+function HandIcon({ value }) {
+  return <img src={IMAGES[value]} alt={value} />;
+}
+
+export default HandIcon;
