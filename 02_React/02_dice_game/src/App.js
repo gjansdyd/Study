392f64442d3d3273550c85d60ec4312ca514b440
MunYong <gjansdyd@gmail.com>
2022-12-26 15:33:06 +0900

    .

--- 02_React/02_dice_game/src/App.js
+++ 02_React/02_dice_game/src/App.js
@@ -1,10 +1,19 @@
+import Button from './Button';
 import Dice from './Dice';
 
 // 아래와 같이 하면 컴포넌트 만들기 완성
 function App() {
   return (
-    <div>
-      <Dice color="red" num= {1}/>
+    <div >
+      <div>
+        {/* 컴포넌트 함수에서 따로 가공하지 않고 
+        단순히 보여주기만 할 모습은 이렇게 children prop을 사용할 수 있음
+        */}
+        <Button>"던지기"</Button>
+        <Button>"처음부터"</Button>
+      </div>
+        <Dice color="red" num= {1}/>
+      
     </div>
   );
 }
